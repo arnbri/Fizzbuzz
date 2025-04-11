0b6c6dfae94efb9d0e453220c354e74a713290f1
arnbri <brighton.a@northeastern.edu>
2025-04-11 10:43:55 +0100

    adds working unittests

---
+++ cm_to_feet_converter.py
@@ -0,0 +1,5 @@
+def convert_cm_to_feet(cm_value):
+    if cm_value < 0:
+        return "Cannot convert negative values"
+    else:
+        return cm_value*0.0328
--- test_unittest.py
+++ test_unittest.py
@@ -1,8 +1,17 @@
 import unittest
+from cm_to_feet_converter import convert_cm_to_feet
 
 class TestUnitTest(unittest.TestCase):
     def test_tests_work(self):
         self.assertEqual(2+2,4)
+
+    def test_cm_converter_happy(self):
+        self.assertEqual(convert_cm_to_feet(1), 0.0328)
+        self.assertNotEqual(convert_cm_to_feet(5),100)
+
+    def test_cm_converter_unhappy(self):
+        self.assertNotEqual(convert_cm_to_feet(5),100, "5cm should not equal 100 feet")
+        self.assertEqual(convert_cm_to_feet(-1), "Cannot convert negative values")
 
 if __name__ == "__main__":
     unittest.main()
